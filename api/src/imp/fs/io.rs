0cb58086ba51ecf4e677a52418b326a5abf1426c
Asakura Mizu <asakuramizu111@gmail.com>
2025-05-11 00:53:41 +0800

    feat: readv & pread64

--- api/src/imp/fs/io.rs
+++ api/src/imp/fs/io.rs
@@ -23,6 +23,49 @@ pub fn sys_read(fd: i32, buf: UserPtr<u8>, len: usize) -> LinuxResult<isize> {
     Ok(get_file_like(fd)?.read(buf)? as _)
 }
 
+pub fn sys_readv(fd: c_int, iov: UserPtr<iovec>, iocnt: usize) -> LinuxResult<isize> {
+    if !(0..=1024).contains(&iocnt) {
+        return Err(LinuxError::EINVAL);
+    }
+
+    let iovs = iov.get_as_mut_slice(iocnt)?;
+    let mut ret = 0;
+    for iov in iovs {
+        if iov.iov_len == 0 {
+            continue;
+        }
+        let buf = UserPtr::<u8>::from(iov.iov_base as usize);
+        let buf = buf.get_as_mut_slice(iov.iov_len as _)?;
+        debug!(
+            "sys_readv <= fd: {}, buf: {:p}, len: {}",
+            fd,
+            buf.as_ptr(),
+            buf.len()
+        );
+
+        let read = get_file_like(fd)?.read(buf)?;
+        ret += read as isize;
+
+        if read < buf.len() {
+            break;
+        }
+    }
+
+    Ok(ret)
+}
+
+pub fn sys_pread64(fd: c_int, buf: UserPtr<u8>, len: usize, offset: u64) -> LinuxResult<isize> {
+    let buf = buf.get_as_mut_slice(len)?;
+    debug!(
+        "pread64 <= fd: {}, buf: {:p}, len: {}, offset: {}",
+        fd,
+        buf.as_ptr(),
+        buf.len(),
+        offset
+    );
+    Ok(File::from_fd(fd)?.inner().read_at(offset, buf)? as _)
+}
+
 /// Write data to the file indicated by `fd`.
 ///
 /// Return the written size if success.
--- src/syscall.rs
+++ src/syscall.rs
@@ -50,8 +50,15 @@ fn handle_syscall(tf: &mut TrapFrame, syscall_num: usize) -> isize {
 
         // io
         Sysno::read => sys_read(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
+        Sysno::readv => sys_readv(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
         Sysno::write => sys_write(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
         Sysno::writev => sys_writev(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
+        Sysno::pread64 => sys_pread64(
+            tf.arg0() as _,
+            tf.arg1().into(),
+            tf.arg2() as _,
+            tf.arg3() as _,
+        ),
         Sysno::lseek => sys_lseek(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _),
 
         // fs mount
